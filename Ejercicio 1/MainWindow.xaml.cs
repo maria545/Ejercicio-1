046ef5c4b5b274b658f7f6d6d2603e2d521b8c78
Maria <Maria@DESKTOP-G07TP2O>
2020-10-20 17:12:19 +0200

    He subido el ejercicio 1

---
+++ Ejercicio 1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio_1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random generador;
+        int contador = 0;
+        int numeroElegido;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void GenerarNumero()
+        {
+            generador = new Random();
+            numeroElegido = generador.Next(0, 100);
+        }
+        private void ComprobarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            if(contador == 0)
+            {
+                GenerarNumero();
+                contador++;
+            }
+            if (numeroElegido > Convert.ToInt32(EscribirTextBox.Text))
+            {
+                PistaTextBlock.Text = "Te has quedado corto";
+            }
+            else if (numeroElegido < Convert.ToInt32(EscribirTextBox.Text))
+            {
+                PistaTextBlock.Text = "Te has pasado";
+            }
+            else
+            {
+                PistaTextBlock.Text = "Has acertado!";
+                contador = 0;
+            }
+
+        }
+
+        private void ReiniciarBoton_Click(object sender, RoutedEventArgs e)
+        {
+            EscribirTextBox.Text = "";
+            GenerarNumero();
+            contador++;
+        }
+    }
+}
